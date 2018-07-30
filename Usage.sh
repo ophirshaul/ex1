89e99e32ff3691f701c04e843f705d43816653f8
ophirshaul <41902920+ophirshaul@users.noreply.github.com>
2018-07-30 15:38:20 +0300

    Create Usage.sh

---
+++ Usage.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+touch vs.tmp
+printf "Checking Volume Usage Size - "
+VS=`df -h|tail +2 |awk '{print $5,$6}' |sed 's/\%//g' > vs.tmp`
+for i in `awk '{if ($1 >=85) print $2,"  ",$1"%"}' vs.tmp`;
+do
+                        if [ ! -z "$i" ] ; then
+                echo "Volume high Usage *** $i \n"
+#                printf "Not OK\n"
+        fi
+
+done
